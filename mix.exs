cbc583646e57f9892cfc0392c5e79f4e10788607
Justin Edwards <justin@igrowbeards.com>
2024-10-12 16:52:32 -0500

    bump to 0.1.2 - add ancestries

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShadowdarkTables.MixProject do
   def project do
     [
       app: :shadowdark_tables,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.16",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
