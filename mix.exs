b1ba666ade6eb318c29170952699b5b0c48959b5
Justin Edwards <justin@igrowbeards.com>
2024-10-12 15:37:20 -0500

    updates for hex.pm

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule ShadowdarkTables.MixProject do
       app: :shadowdark_tables,
       version: "0.1.0",
       elixir: "~> 1.16",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Postgrex",
+      source_url: "https://github.com/igrowbeards/ShadowdarkTables"
     ]
   end
 
@@ -22,7 +27,21 @@ defmodule ShadowdarkTables.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 1.0", only: [:dev, :test]},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    "Provides roll tables for the Shadowdark RPG"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/igrowbeards/ShadowdarkTables"}
     ]
   end
 end
